5f0fd2843c36e9793455e1fc7d48284010715fef
Simon K <simshard@gmail.com>
2026-01-11 13:47:13 +0000

    setup tweaked

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import './app.css'
+const tech = "React, Tailwind and Vite";
 
-const app = document.getElementById('app')
-const tech='Reakt und Vril'
-let root = ReactDOM.createRoot(app)
-
-function App() {
-  return <h1 className="text-3xl font-bold underline text-red-700">Hello { tech}!</h1>
+export function App() {
+  return <h1 className="font-mono text-4xl">Hello {tech}!</h1>;
 }
-
-root.render(<App />)
---
+++ src/entry.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./app.css";
+import { App } from "./app.jsx";
+
+createRoot(document.getElementById("app")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
