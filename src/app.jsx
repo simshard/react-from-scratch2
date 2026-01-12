c0e8cae9e50af1aaef28c50954a243f6f0d17aa5
Simon K <simshard@gmail.com>
2026-01-12 17:51:54 +0000

    styles and app components

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,10 @@ import { Header } from "./components/Header";
 
 export function App() {
   return (
-    // <PageWrapper>
-    //   <Container>
-    //   </Container>
-    // </PageWrapper>
-    <main className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">
-      <div className="mx-auto max-w-5xl p-4 md:p-8">
+  <PageWrapper>
+      <Container>
         <Header />
-      </div>
-    </main>
+      </Container>
+  </PageWrapper>
   );
 }
