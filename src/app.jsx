18ef291d02a946cf1c92a61f649eceae0f82494e
Simon K <simshard@gmail.com>
2026-01-13 00:06:05 +0000

    static ui layout

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,32 @@ import { PageWrapper } from "./components/PageWrapper";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
 
+import { Search } from "./components/Search";
+import { Shortlist } from "./components/Shortlist"
+import { PuppiesList } from "./components/PuppiesList";
+import { NewPuppyForm } from "./components/NewPuppyForm";
+
 export function App() {
   return (
   <PageWrapper>
       <Container>
         <Header />
+        <Main />
       </Container>
   </PageWrapper>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <div className="mt-24 grid gap-8 sm:grid-cols-2 mb-6">
+        <Search />
+        <Shortlist /> 
+      </div>
+      <PuppiesList />
+      <NewPuppyForm />
+       {/* */}
+    </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 export function Header() {
   return (
-
         <header>
-   
           <a className="group" href="/">
             <div className="inline-flex items-center gap-4">
               <img
@@ -10,11 +8,11 @@ export function Header() {
                 alt="Flower"
                 className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
               />
-              <p className="text-lg font-semibold">Plantpix</p>
+              <p className="text-lg font-semibold">Planetpix</p>
             </div>
           </a>
           <div className="mt-6">
-            <h1 className="text-lg font-bold">Nice Flowers</h1>
+            <h1 className="text-lg font-bold">groovy stuff</h1>
             <p className="text-slate-600">
               Don't take our word — let the pictures do the talking !
             </p>
